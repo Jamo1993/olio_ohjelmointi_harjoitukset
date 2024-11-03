bb880de05938d44789ccec35a20b0271ea41ed1e
Jamo <jamo.jaakola@gmail.com>
2024-11-03 17:39:34 +0200

    harjoitus_1_valmis

---
+++ harjoitus_1/myFunctions.h
@@ -0,0 +1,13 @@
+#ifndef MYFUNCTIONS_H
+#define MYFUNCTIONS_H
+using namespace std;
+//Header tiedostoissa on mikä funktion palautusarvon tyyppi,
+//ja mitä palautusarvoja se mahdollisesti ottaa vastaan..
+
+void calcSum(int a, int b);
+void calcDiv(int a, int b);
+int retSum(int a, int b);
+float retDiv(int a, int b);
+
+
+#endif // MYFUNCTIONS_H
